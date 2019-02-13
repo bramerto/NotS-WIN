de6faf1b2688f7e737916a297eb6f09aa0c46c54
Bram <bramerto@gmail.com>
2019-02-13 21:26:32 +0100

    Listening functionality

--- MultiChat/MultiChat.cs
+++ MultiChat/MultiChat.cs
@@ -8,10 +8,59 @@ namespace MultiChat
 {
     public partial class MultiChat : Form
     {
+        TcpClient tcpClient;
+        NetworkStream networkStream;
+        Thread thread;
 
         public MultiChat()
         {
             InitializeComponent();
         }
+
+        public delegate void setMessage(string input);
+
+        private void testMethod(string input)
+        {
+            if (this.ChatBox.InvokeRequired)
+            {
+                // var test = testDelegate(string);
+                // this.Invoke(test, new object[], input);
+            }
+            else
+            {
+                // this.update ?
+            }
+        }
+
+        private void ListenBtn_Click(object sender, EventArgs e)
+        {
+            var server = new TcpListener(IPAddress.Any, 9000);
+            server.Start();
+            
+            ChatBox.AppendText("Listening for client..." + Environment.NewLine);
+
+            Console.WriteLine("Connecting...");
+            tcpClient = server.AcceptTcpClient();
+
+            //thread = new Thread(ReceiveData());
+            //thread.Start();
+        }
+
+        //private ThreadStart ReceiveData()
+        //{
+        //    int value;
+        //    string text;
+        //    byte[] arr = new byte[1024];
+
+        //    networkStream = tcpClient.GetStream();
+
+        //    while (true)
+        //    {
+        //       networkStream.Read(arr, 0, arr.Length);
+        //       text = Encoding.ASCII.GetString(arr);
+
+        //       if (text == "bye") break;
+        //    }
+        //}
     }
 }
