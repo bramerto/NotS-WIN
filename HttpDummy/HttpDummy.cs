439cbce8ad5e5447c2b95a44f8d366ee93202d19
Bram <bramerto@gmail.com>
2019-03-06 15:35:01 +0100

    Added simple HTTP server
    To test the proxy

---
+++ HttpDummy/HttpDummy.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace HttpDummy
+{
+    class HttpDummy
+    {
+        static void Main(string[] args)
+        {
+            HttpListener listener;
+
+            try
+            {
+                listener = new HttpListener();
+                listener.Prefixes.Add("http://localhost:4000/dummy/");
+                listener.Start();
+                Console.WriteLine("Listening...");
+
+                while (true)
+                {
+                    var context = listener.GetContext();
+                    string msg = "Hello World!";
+                    context.Response.ContentLength64 = Encoding.UTF8.GetByteCount(msg);
+                    context.Response.StatusCode = (int)HttpStatusCode.OK;
+                    using (Stream stream = context.Response.OutputStream)
+                    {
+                        using (StreamWriter writer = new StreamWriter(stream))
+                        {
+                            writer.Write(msg);
+                        }
+                    }
+
+                    Console.WriteLine("Message sent (HTTP 200 OK)");
+                }
+            }
+            catch (WebException ex)
+            {
+                Console.WriteLine("Something went wrong (HTTP 500 Internal Server Error)");
+                Console.WriteLine(ex.Status);
+            }
+        }
+    }
+}
