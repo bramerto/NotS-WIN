31fb373adc5efe3f867aa916b9ac69d488a89b00
Bram <bramerto@gmail.com>
2019-03-06 15:35:35 +0100

    Added controls
    And events

--- ProxyServer/MainWindow.xaml.cs
+++ ProxyServer/MainWindow.xaml.cs
@@ -10,15 +10,53 @@ namespace ProxyServer
             InitializeComponent();
         }
 
+        //Form events
         private void FormSizeChanged(object sender, SizeChangedEventArgs e)
         {
             LayoutGrid.Width = e.NewSize.Width;
             LayoutGrid.Height = e.NewSize.Height;
         }
 
+        //Button events
         private void StartServer(object sender, RoutedEventArgs e)
         {
             Console.WriteLine("Server started");
+        }
+
+        private void StopServer(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SetBufferSize(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        //Checkbox events
+        private void EnableCache(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void EnableImageFilter(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void EnableAdFilter(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void EnablePrivacyFilter(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void EnableScriptFilter(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
