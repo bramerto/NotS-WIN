e630069817304428195107438649b93b5ac66bb6
Bram <bramerto@gmail.com>
2019-03-19 16:24:08 +0100

    Set response class

--- Proxy/Messages/HttpResponse.cs
+++ Proxy/Messages/HttpResponse.cs
@@ -1,6 +1,23 @@
-﻿namespace Proxy.Messages
+﻿using System.Collections;
+
+namespace Proxy.Messages
 {
     class HttpResponse : IHttpMessage
     {
+        public string Message;
+
+        public int Status;
+        public string Version;
+        public Hashtable Headers;
+        public int BodySize;
+        public string BodyData;
+
+        public enum State
+        {
+            OK                    = 200,
+            BAD_REQUEST           = 400,
+            NOT_FOUND             = 404,
+            INTERNAL_SERVER_ERROR = 500
+        }
     }
 }
