75a07b919bbb328bc97dec3a3fd1664fb4980439
Bram <bramerto@gmail.com>
2020-05-06 13:42:31 +0200

    shortened functions

--- LINQ-Demo/Extension/LinqExtensionImplementation.cs
+++ LINQ-Demo/Extension/LinqExtensionImplementation.cs
@@ -9,34 +9,23 @@ namespace LINQ_Demo.Extension
         {
             Console.WriteLine("Bram's 'Where' implementation");
             foreach (var item in source)
-            {
-                if (predicate(item))
-                {
+                if (predicate(item)) 
                     yield return item;
-                }
-            }
         }
 
         public static IEnumerable<TResult> Select<TSource, TResult>(this IEnumerable<TSource> source, Func<TSource, TResult> selector)
         {
             Console.WriteLine("Bram's 'Select' implementation");
             foreach (var item in source)
-            {
                 yield return selector(item);
-            }
         }
 
         public static bool Any<T>(this IEnumerable<T> source, Func<T, bool> predicate)
         {
             Console.WriteLine("Bram's 'Any' implementation");
             foreach (var item in source)
-            {
-                if (predicate(item))
-                {
+                if (predicate(item)) 
                     return true;
-                }
-            }
-
             return false;
         }
     }
